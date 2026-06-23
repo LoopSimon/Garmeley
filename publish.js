d2e041e4bd3d9f3e8a9362252575296ef1d50612
LoopSimon <simonks@protonmail.com>
2026-06-23 20:36:29 +0200

    vault backup: 2026-06-23 20:36:29

---
+++ publish.js
@@ -0,0 +1,17 @@
+var analyticsScript = document.createElement('script');
+analyticsScript.defer = true;
+analyticsScript.setAttribute('data-domain', 'https://publish.obsidian.md/garmeley');
+analyticsScript.src = 'https://plausible.io/js/plausible.js';
+document.head.appendChild(analyticsScript);
+
+var plausibleScript = document.createElement('script');
+plausibleScript.async = true;
+plausibleScript.src = 'https://plausible.io/js/pa-cWwjMjmnSKaXQDhQiZdMs.js';
+document.head.appendChild(plausibleScript);
+
+var plausibleInit = document.createElement('script');
+plausibleInit.textContent = `
+  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()
+`;
+document.head.appendChild(plausibleInit);
